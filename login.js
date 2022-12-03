a29386ad45810728c23b799d1afd96be5d0be2ac
jeffjeff023 <jeffacula@gmail.com>
2022-12-03 15:27:13 +0800

    Initial Project Version

---
+++ login.js
@@ -0,0 +1,9 @@
+const validate = () => {
+    const user = document.getElementById('user');
+    const password = document.getElementById('password')
+    const submit = document.getElementById('submit')
+
+    if (user == 'joffrey' && password == 'acula') {
+        window.open (app.html)
+    }
+}
